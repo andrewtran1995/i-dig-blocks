5c659e865fb5465c1b3f1c38d994181aa794593c
Andrew Tran <andrew.tran345@gmail.com>
2020-08-21 22:42:00 -0700

    Working file

---
+++ main.py
@@ -0,0 +1,20 @@
+import configparser
+import discord
+
+
+def main():
+    config = configparser.ConfigParser()
+    config.read("project.properties")
+    discord_bot_token = config.get("Discord", "BotToken")
+
+    client = discord.Client()
+
+    @client.event
+    async def on_ready():
+        print(f'${client.user} has connected to Discord!')
+
+    client.run(discord_bot_token)
+
+
+if __name__ == '__main__':
+    main()
